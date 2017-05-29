4fd397931c003d5dea2ccb5c98ff2c00910d96c4
Vedpathak <omkarv@amazon.com>
2017-05-29 09:32:12 -0700

    Completed questions 1 - 4

--- search.py
+++ search.py
@@ -71,20 +71,49 @@ def depthFirstSearch(problem):
   """
   Search the deepest nodes in the search tree first
   [2nd Edition: p 75, 3rd Edition: p 87]
-  
+
   Your search algorithm needs to return a list of actions that reaches
-  the goal.  Make sure to implement a graph search algorithm 
+  the goal.  Make sure to implement a graph search algorithm
   [2nd Edition: Fig. 3.18, 3rd Edition: Fig 3.7].
-  
+
   To get started, you might want to try some of these simple commands to
   understand the search problem that is being passed in:
-  
+
   print "Start:", problem.getStartState()
   print "Is the start a goal?", problem.isGoalState(problem.getStartState())
   print "Start's successors:", problem.getSuccessors(problem.getStartState())
   """
-  "*** YOUR CODE HERE ***"
-  util.raiseNotDefined()
+
+  # initialize frontier
+  frontier = util.PriorityQueue()
+  frontier.push(([problem.getStartState()], []), 0)
+  # ensure we do graph search
+  explored = {}
+  frontier_visited = {}
+
+  while True:
+      if frontier.isEmpty():
+          return Exception
+
+      frontier_path, prev_actions = frontier.pop()
+      frontier_end = frontier_path[-1]
+      explored[frontier_end] = True
+
+      if problem.isGoalState(frontier_end):
+          return prev_actions
+
+      frontier_actions = problem.getSuccessors(frontier_end)
+
+      for action in frontier_actions:
+          next_state, direction, cost = action
+          new_frontier_path = frontier_path[:]
+          new_actions = prev_actions[:]
+          new_frontier_path.append(next_state)
+          new_actions.append(direction)
+
+          if (explored.get(next_state, False) is False and frontier_visited.get(next_state, False) is False):
+              frontier_visited[next_state] = True
+              frontier.push((new_frontier_path, new_actions), cost)
 
 def breadthFirstSearch(problem):
   """
@@ -92,12 +121,74 @@ def breadthFirstSearch(problem):
   [2nd Edition: p 73, 3rd Edition: p 82]
   """
   "*** YOUR CODE HERE ***"
-  util.raiseNotDefined()
-      
+
+  # initialize frontier
+  frontier = util.Queue()
+  frontier.push(([problem.getStartState()], []))
+  # ensure we do graph search
+  explored = {}
+  frontier_visited = {}
+
+  while True:
+      if frontier.isEmpty():
+          return Exception
+
+      frontier_path, prev_actions = frontier.pop()
+      frontier_end = frontier_path[-1]
+      explored[frontier_end] = True
+
+      if problem.isGoalState(frontier_end):
+          return prev_actions
+
+      frontier_actions = problem.getSuccessors(frontier_end)
+
+      for action in frontier_actions:
+          next_state, direction, cost = action
+          new_frontier_path = frontier_path[:]
+          new_actions = prev_actions[:]
+          new_frontier_path.append(next_state)
+          new_actions.append(direction)
+
+          if (explored.get(next_state, False) is False and frontier_visited.get(next_state, False) is False):
+              frontier_visited[next_state] = True
+              frontier.push((new_frontier_path, new_actions))
+
+
 def uniformCostSearch(problem):
-  "Search the node of least total cost first. "
-  "*** YOUR CODE HERE ***"
-  util.raiseNotDefined()
+    """
+    Search the node of least total cost first.
+    """
+
+    # initialize frontier
+    frontier = util.PriorityQueue()
+    frontier.push(([problem.getStartState()], []), problem.getCostOfActions([]))
+    # ensure we do graph search
+    explored = {}
+    frontier_visited = {}
+
+    while True:
+        if frontier.isEmpty():
+            return Exception
+
+        frontier_path, prev_actions = frontier.pop()
+        frontier_end = frontier_path[-1]
+        explored[frontier_end] = True
+
+        if problem.isGoalState(frontier_end):
+            return prev_actions
+
+        frontier_actions = problem.getSuccessors(frontier_end)
+
+        for action in frontier_actions:
+            next_state, direction, cost = action
+            new_frontier_path = frontier_path[:]
+            new_actions = prev_actions[:]
+            new_frontier_path.append(next_state)
+            new_actions.append(direction)
+
+            if (explored.get(next_state, False) is False and frontier_visited.get(next_state, False) is False):
+                frontier_visited[next_state] = True
+                frontier.push((new_frontier_path, new_actions), problem.getCostOfActions(new_actions))
 
 def nullHeuristic(state, problem=None):
   """
@@ -108,10 +199,40 @@ def nullHeuristic(state, problem=None):
 
 def aStarSearch(problem, heuristic=nullHeuristic):
   "Search the node that has the lowest combined cost and heuristic first."
-  "*** YOUR CODE HERE ***"
-  util.raiseNotDefined()
-    
-  
+  # initialize frontier
+  frontier = util.PriorityQueue()
+  initial_cost = problem.getCostOfActions([]) + heuristic(problem.getStartState(), problem)
+  frontier.push(([problem.getStartState()], []), problem.getCostOfActions([]))
+  # ensure we do graph search
+  explored = {}
+  frontier_visited = {}
+
+  while True:
+      if frontier.isEmpty():
+          return Exception
+
+      frontier_path, prev_actions = frontier.pop()
+      frontier_end = frontier_path[-1]
+      explored[frontier_end] = True
+
+      if problem.isGoalState(frontier_end):
+          return prev_actions
+
+      frontier_actions = problem.getSuccessors(frontier_end)
+
+      for action in frontier_actions:
+          next_state, direction, cost = action
+          new_frontier_path = frontier_path[:]
+          new_actions = prev_actions[:]
+          new_frontier_path.append(next_state)
+          new_actions.append(direction)
+
+          if (explored.get(next_state, False) is False and frontier_visited.get(next_state, False) is False):
+              frontier_visited[next_state] = True
+              new_total_cost = problem.getCostOfActions(new_actions) + heuristic(next_state, problem)
+              frontier.push((new_frontier_path, new_actions), new_total_cost)
+
+
 # Abbreviations
 bfs = breadthFirstSearch
 dfs = depthFirstSearch
